13c06400ab5b67faef163efbbd9f905d3c2cee93
Qetz <general@qetz.de>
2022-08-27 21:39:36 +0200

    Extract photo container into own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import Photo, {PhotoProperties} from "../components/Photo";
 import path from "path";
 import * as fs from "fs";
+import PhotoContainer from "../components/PhotoContainer";
+import React from "react";
 
 type Photos = (PhotoProperties & {id: number})[]
 
 export default function IndexPage({photos}: {photos: Photos}) {
   return (
-    <div className="flex justify-center my-5">
-      <div className="grid grid-cols-2 lg:grid-cols-3 grid-flow-row-dense gap-x-8 gap-y-4">
-        {photos.map(photo => (
-          <Photo key={photo.id} {...photo}/>
-        ))}
-      </div>
-    </div>
+    <PhotoContainer>
+      {photos.map(photo => (
+        <Photo key={photo.id} {...photo}/>
+      ))}
+    </PhotoContainer>
   )
 }
 
