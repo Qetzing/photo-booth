c001418d58b116f7a6e4b6efdb132b14419799b8
Qetz <general@qetz.de>
2022-08-28 00:25:19 +0200

    Improve device compatibility with the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,12 +4,12 @@ export default function Footer() {
   return (
     <div className="w-full mt-auto bg-gray-900 text-white py-4">
       <div className="flex flex-col">
-        <div className="">
-          <p className="ml-20 inline">&copy; 2022</p>
+        <div className="flex justify-center sm:justify-start">
+          <p className="sm:ml-20 inline">&copy; 2022</p>
           <p className="ml-3 inline"><a href={imprintUrl}>Imprint</a></p>
         </div>
-        <div className="flex justify-end mt-1.5">
-          <p className="mr-20">
+        <div className="flex justify-center sm:justify-end mt-1.5">
+          <p className="sm:mr-20">
             Made by
             <a href="https://qetz.de"> Qetz </a>
             with {'\u2764'}
