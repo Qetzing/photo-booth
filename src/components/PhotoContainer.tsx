439d681114944d8115a0edddab31e5b5b2ea4334
Qetz <general@qetz.de>
2022-08-27 22:59:37 +0200

    Add hr for photo container

--- src/components/PhotoContainer.tsx
+++ src/components/PhotoContainer.tsx
@@ -9,6 +9,7 @@ export default function PhotoContainer(properties: PhotoContainerProperties) {
   return (
     <div id={properties.title} className="mt-20 flex flex-col items-center">
       <span className="mb-5 text-2xl text-gray-400 font-medium tracking-widest">{properties.title}</span>
+      <hr className="border-2 bg-slate-400 w-1/2 mt-1 mb-3"/>
       <div className="flex justify-center my-5">
         <div className="grid grid-cols-2 lg:grid-cols-3 grid-flow-row-dense gap-x-8 gap-y-4">
           {properties.children}
