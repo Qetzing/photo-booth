d52f8e045991d1a487e3dfd21d10fb0b4560267c
Qetz <general@qetz.de>
2022-08-27 19:17:28 +0200

    Add static photo extraction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,40 @@
-import Photo from "../components/Photo";
+import Photo, {PhotoProperties} from "../components/Photo";
+import path from "path";
+import * as fs from "fs";
 
-export default function IndexPage() {
+type Photos = (PhotoProperties & {id: number})[]
+
+export default function IndexPage({photos}: {photos: Photos}) {
   return (
     <div className="flex justify-center my-5">
       <div className="grid grid-cols-2 lg:grid-cols-3 grid-flow-row-dense gap-x-8 gap-y-4">
-
+        {photos.map(photo => (
+          <Photo key={photo.id} {...photo}/>
+        ))}
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const directory = path.join(process.cwd(), 'data')
+  const data = fs.readFileSync(directory + '/photos.json', 'utf8')
+  const photos: Photos = []
+
+  for (const photo of JSON.parse(data).photos) {
+    photos.push({
+      id: photo.id,
+      src: photo.src,
+      alt: photo.alt,
+      title: photo.title,
+      people: photo.people,
+      size: photo.size
+    })
+  }
+
+  return {
+    props: {
+      photos: photos
+    }
+  }
 }
